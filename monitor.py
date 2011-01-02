6b8abc51c55e58b1b4de73135011ddb3d27c0f77
Pham Cong Dinh <pcdinh@gmail.com>
2011-01-02 06:36:39 -0800

    More modern code for calling parent class constructor. See http://stackoverflow.com/questions/753640/inheritance-and-overriding-init-in-python

--- monitor.py
+++ monitor.py
@@ -37,7 +37,7 @@ class ModuleMonitor(threading.Thread):
     """Monitor module source file changes"""
 
     def __init__(self, interval=1):
-        threading.Thread.__init__(self)
+        super(self.__class__, self).__init__()
         self.daemon = True
         self.mtimes = {}
         self.queue = queue.Queue()
